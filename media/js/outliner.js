fba8332c934fd05aaf40e34473f582f084b3c8d4
condy0919 <condy0919@gmail.com>
2014-08-11 13:19:29 +0800

    Update outliner.js

--- media/js/outliner.js
+++ media/js/outliner.js
@@ -21,7 +21,7 @@ $(function() {
     });
     
     $(window).scroll(function () { 
-        if ($(window).scrollTop() == $(document).height() - $(window).height()) { 
+        if ($(window).scrollTop() >= $('#container').position().top { 
             alert('bottom!!'); 
         } 
     });
